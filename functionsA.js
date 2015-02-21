0885789ce2d2720f9a0ee47e4fee3c3ad897370f
Michael Wiss <michael.wiss@gmail.com>
2015-02-21 16:56:03 -0600

    IIFFE

--- functionsA.js
+++ functionsA.js
@@ -7,3 +7,11 @@ function getSize(width, height, depth) {
 var areaOne = getSize(3, 3, 3)[0];
 var volumeOne = getSize(3, 2 3)[1];
 
+//immediately invoked function expressions IIFFE
+
+var area = ( function() {
+	var width = 3;
+	var height = 2;
+	return width * height;
+} () );
+

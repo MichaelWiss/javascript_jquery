07efc7fe89748bd10f79ba37d688775cec3f8e37
Michael Wiss <michael.wiss@gmail.com>
2015-02-25 11:29:27 -0600

    select an element from the NodeList

--- DOM.js
+++ DOM.js
@@ -3,3 +3,12 @@ var el = document.getElementById('one');
 
 // Change the value of the class attribute.
 el.className = 'cool';
+
+
+
+var elements = document.getElementsByClassName('hot');
+  if (elements.length >= 1) {
+      var firstItem = elements.item(0);
+  }
+
+  

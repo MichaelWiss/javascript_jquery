9e70453d74a9cad825c56dd84376e75145f0c8f2
Michael Wiss <michael.wiss@gmail.com>
2015-02-26 17:35:35 -0600

    check for an attribute

--- DOM.js
+++ DOM.js
@@ -5,10 +5,23 @@ var el = document.getElementById('one');
 el.className = 'cool';
 
 
-
+//item() method
 var elements = document.getElementsByClassName('hot');
   if (elements.length >= 1) {
       var firstItem = elements.item(0);
   }
 
-  
+//array syntax
+var elements = document.getElementsByClassName('hot');
+  if (elements.length >= 1) {
+      var firstItem = elements[0];
+  }
+
+  // checking for attributes
+  var firstItem = document.getElementById('one');  //get first li
+
+  if (firstItem.hasAttribute('class')) {  //if it has class atttribute
+      var attr = firstItem.getAttribute('class'); //get the attribute
+     //add the value of the attribute after the list
+    var el = document.getElementById('scriptResults');
+    el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';

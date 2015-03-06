5ac49683cf0c07a79c16b567cfc20a8c5da66a23
Michael Wiss <michael.wiss@gmail.com>
2015-03-05 22:16:51 -0600

    String()

--- DOM.js
+++ DOM.js
@@ -122,3 +122,20 @@ if (elUsername.addEventListener) {           // If event listener supported
     }
   }
 </script>
+
+//String() method, 0 pad method
+
+function zeroPad(number, width) {
+  var string = String(number);
+  while (string.length < width)
+    string = "0" + string;
+  return string;
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+  console.log(zeroPad(cows, 3) + " Cows");
+  console.log(zeroPad(chickens, 3) + " Chickens");
+  console.log(zeroPad(pigs, 3) + " Pigs");
+}
+
+printFarmInventory(7, 16, 3);

b6bbd33eab88337d438724e406a19e5dd71b9689
Michael Wiss <michael.wiss@gmail.com>
2015-03-11 21:29:26 -0500

    jquery get content

--- DOM.js
+++ DOM.js
@@ -196,3 +196,16 @@ window.addEventListener('beforeunload', function(event) {
   (event || window.event).returnValue = message;
   return message;
 });
+
+
+//changing content Jquery
+
+$(function() {
+  $('li:contains("pine")').text('almonds');
+  $('li.hot').html(function() {
+    return '<em>' + $(this).text() + '</em>';
+  });
+  $('li#one').remove();
+});
+
+

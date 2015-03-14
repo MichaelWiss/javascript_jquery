93d735f19cd94bfa939d3dac51369fa34aed41da
Michael Wiss <michael.wiss@gmail.com>
2015-03-14 12:52:47 -0500

    jquery event object

--- DOM.js
+++ DOM.js
@@ -235,3 +235,18 @@ $(function() {
     'padding-left': '+=75'
   });
 });
+
+
+//jquery event object
+
+$(function() {
+
+  $('li').on('click', function(e) {
+    $('li span').remove();
+    var date = new Date();
+    date.setTime(e.timeStamp);
+    var clicked = date.toDateString();
+    $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
+  });
+
+});

399ec74fff0833d9d833df1a2ee6ae58509d39b4
Michael Wiss <michael.wiss@gmail.com>
2015-03-14 14:12:10 -0500

    basic effects

--- DOM.js
+++ DOM.js
@@ -250,3 +250,19 @@ $(function() {
   });
 
 });
+
+//basic effects
+
+$(function() {
+
+  $('h2').hide().slideDown();
+  var $li = $('li');
+  $li.hide().each(function(index) {
+    $(this).delay(700 * index).fadeIn(700);
+  });
+
+  $li.on('click', function() {
+    $(this).fadeOut(700);
+  });
+
+});

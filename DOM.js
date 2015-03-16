2b193669560ee94690d9ffa37ecc15b19d5e0907
Michael Wiss <michael.wiss@gmail.com>
2015-03-16 16:10:51 -0500

    using index numbers

--- DOM.js
+++ DOM.js
@@ -283,3 +283,10 @@ $(function() {
   });
 
 });
+//using index numbers
+
+$(function() {
+  $('li:lt(2)').removeClass('hot');
+  $('li').eq(0).addClass('complete');
+  $('li:gt(2)').addClass('cool');
+});

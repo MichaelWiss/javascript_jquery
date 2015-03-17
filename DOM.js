1efaceab141082dfc6cc675c2472b2be30e5441a
Michael Wiss <michael.wiss@gmail.com>
2015-03-17 13:08:08 -0500

    determining position of items on the page

--- DOM.js
+++ DOM.js
@@ -290,3 +290,24 @@ $(function() {
   $('li').eq(0).addClass('complete');
   $('li:gt(2)').addClass('cool');
 });
+
+
+
+//determining position of items on the page
+
+$(function() {
+  var $window = $(window);
+  var $slideAd = $('#slideAd');
+  var endZone = $('#footer').offset().top - $window.height() - 500;
+
+  $window.on('scroll', function() {
+
+    if (endZone < $window.scrollTop()) {
+      $slideAd.animate({ 'right': '0px' }, 250);
+    } else {
+      $slideAd.stop(true).animate({ 'right': '-360px' }, 250);
+    }
+
+  });
+
+});

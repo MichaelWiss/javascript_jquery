4b421d1e77b27845fe2ae901f18d146d2737eae1
Michael Wiss <michael.wiss@gmail.com>
2015-03-21 20:21:23 -0500

    controlling ajax

--- DOM.js
+++ DOM.js
@@ -368,3 +368,34 @@ $('#register').on('submit', function(e) {           // When form is submitted
     $('#register').html(data);                    // Where to display result
   });
 });
+
+//controlling ajax
+
+$('nav a').on('click', function(e) {
+  e.preventDefault();
+  var url = this.href;                                      // URL to load
+  var $content = $('#content');                             // Cache selection
+
+  $('nav a.current').removeClass('current');                // Update links
+  $(this).addClass('current');
+  $('#container').remove();                                 // Remove content
+
+  $.ajax({
+    type: "POST",                                           // GET or POST
+    url: url,                                               // Path to file
+    timeout: 2000,                                          // Waiting time
+    beforeSend: function() {                                // Before Ajax 
+      $content.append('<div id="load">Loading</div>');      // Load message
+    },
+    complete: function() {                                  // Once finished
+      $('#load').remove();                               // Clear message
+    },
+    success: function(data) {                               // Show content
+      $content.html( $(data).find('#container') ).hide().fadeIn(400);
+    },
+    fail: function() {                                      // Show error msg 
+      $('#panel').html('<div class="loading">Please try again soon.</div>');
+    }
+  });
+
+});

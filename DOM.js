90fe6aa7666b0bdd7a544c616b3c0a7d8bae1f9f
Michael Wiss <michael.wiss@gmail.com>
2015-03-24 22:39:51 -0500

    localstorage

--- DOM.js
+++ DOM.js
@@ -499,3 +499,23 @@ function fail(msg) {                                        // Not got location
   elMap.textContent = msg;                                  // Show text input
   console.log(msg.code);                                    // Log the error
 }
+
+
+//localstorage
+
+if (Modernizr.localstorage) {
+
+  var txtUsername = document.getElementById('username'); // Get form elements
+  var txtAnswer = document.getElementById('answer');
+
+  txtUsername.value = localStorage.getItem('username');  // Elements populated
+  txtAnswer.value = localStorage.getItem('answer');      // by localStorage data
+
+  txtUsername.addEventListener('input', function () {    // Data saved on keyup
+    localStorage.setItem('username', txtUsername.value);
+  }, false);
+
+  txtAnswer.addEventListener('input', function () {      // Data saved on keyup
+    localStorage.setItem('answer', txtAnswer.value);
+  }, false);
+}

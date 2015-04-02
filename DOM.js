c8ddf6a7afabb0d679f90a6a0599a565be2e5fe7
Michael Wiss <michael.wiss@gmail.com>
2015-04-02 13:25:36 -0500

    modal scripts

--- DOM.js
+++ DOM.js
@@ -645,5 +645,15 @@ $('.tab-list').each(function(){                   // Find lists of tabs
   });
 });
 
+//modal script
+
+(function(){
+  var $content = $('#share-options').detach();   // Remove modal from page
+
+  $('#share').on('click', function() {           // Click handler to open modal
+    modal.open({content: $content, width:340, height:300});
+  });
+}());
+
 
 
